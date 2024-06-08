dff73a381e2bc5b7ea3c103a48b5601406b40ad6
Mr Polarbear <118489656+the-mr-poarbear@users.noreply.github.com>
2024-06-08 18:20:33 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,89 @@
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy import desc
+from sqlalchemy.orm import Session
+from codes import  models, schemas
+from codes.database import SessionLocal, engine
+from fastapi.middleware.cors import CORSMiddleware
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+origins = {
+    "http://localhost:5173",
+    "http://127.0.0.1:5173" 
+}
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+def ReadUsers(db : Session = Depends(get_db)):
+    return db.query(models.Users).all()
+
+
+
+@app.get("/top10")
+def get_top_10(db: Session = Depends(get_db)):
+    query = db.query(models.Users).order_by(desc(models.Users.score)).limit(10)
+    result = query.all()
+    return [{"name": row.name, "score": row.score} for row in result]
+
+
+
+@app.post("/login")
+def Login(name ,  db : Session = Depends(get_db)):
+    user_model = db.query(models.Users).filter(models.Users.name == name).first()
+
+    if user_model is None :
+        new_user = schemas.User(name=name, score=0)
+        CreateUser(new_user , db)
+        return new_user
+    
+    return "Found"
+            
+
+@app.post("/")
+def CreateUser(user : schemas.User , db : Session = Depends(get_db)):
+    user_model = models.Users()
+    user_model.name = user.name 
+    user_model.score = user.score
+    
+    db.add(user_model)
+    db.commit()
+
+    return user
+
+@app.put("/")
+def UpdateUser(name , status ,  db : Session = Depends(get_db) ):
+        user_model = db.query(models.Users).filter(models.Users.name == name).first()
+        
+        if user_model is None :
+             raise HTTPException(
+                  status_code=404,
+                  detail="User Was Not Found"
+             )
+        
+        if status == "WIN" :
+            user_model.score += 1
+        elif status == "LOSE" :
+             user_model.score -= 1
+
+        db.add(user_model)
+        db.commit()
+
+        return user_model
